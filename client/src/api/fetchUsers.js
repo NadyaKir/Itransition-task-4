5f5dbfed5e519f889a8a4011743516e0c9a81d10
Nadezhda Kireyeva <styyypik@gmail.com>
2024-04-17 20:35:49 +0500

    feat: formate dates

--- client/src/api/fetchUsers.js
+++ client/src/api/fetchUsers.js
@@ -1,11 +1,30 @@
 import axios from "axios";
+import moment from "moment";
 
 const fetchUsers = async () => {
   try {
     const response = await axios.get("http://localhost:8000/api/getAllUsers");
-    return response.data;
+    const usersData = response.data;
+
+    const users = usersData.map((userData) => {
+      const formattedRegistrationDate = moment(
+        userData.registrationDate
+      ).format("MMMM Do YYYY, h:mm:ss a");
+      const formattedLastLoginDate = userData.lastLoginDate
+        ? moment(userData.lastLoginDate).format("MMMM Do YYYY, h:mm:ss a")
+        : null;
+
+      return {
+        ...userData,
+        registrationDate: formattedRegistrationDate,
+        lastLoginDate: formattedLastLoginDate,
+      };
+    });
+
+    return users;
   } catch (error) {
     console.error("Error fetching users:", error);
+    throw error;
   }
 };
 
