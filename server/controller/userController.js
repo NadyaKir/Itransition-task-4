3278dedd42c2f600de94b30d748111b9139d7a53
Nadezhda Kireyeva <styyypik@gmail.com>
2024-04-17 17:40:37 +0500

    feat: getAllUsers controller

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -11,7 +11,20 @@ export const create = async (req, res) => {
     const saveData = await userData.save();
     res.status(200).json(saveData);
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ error: error });
+  }
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const userData = await User.find();
+
+    if (!userData) {
+      return res.status(404).json({ msg: "User data not found" });
+    }
+
+    res.status(200).json(userData);
+  } catch (error) {
     res.status(500).json({ error: error });
   }
 };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { create } from "../controller/userController.js";
+import { create, getAllUsers } from "../controller/userController.js";
 
 const route = express.Router();
 
 route.post("/create", create);
+route.get("/getAllUsers", getAllUsers);
 
 export default route;
